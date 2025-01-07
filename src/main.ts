612bad6a325c94c4b4ce31964e4034fe11fed4a3
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2025-01-07 18:01:43 +0545

    feat: add .env.example file and update package.json scripts for improved configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const appConfig = app.get(ConfigService);
+
+  const PORT = appConfig.get('PORT') ?? 7002;
+
+  // CORS Prevention
+  app.enableCors({
+    // Currently Configured to be accessed by *, change as per required
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+
+  await app.listen(PORT);
+  console.log(
+    `Application running on port [${PORT}]\n http://localhost:${PORT}`,
+  );
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
